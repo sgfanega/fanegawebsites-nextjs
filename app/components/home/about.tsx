46f68438000d53fc86ddc66177a4175c7e514caa
sgfanega <steve.fanega.ii@gmail.com>
2024-04-19 20:28:44 -0500

    changed paddings and margins

--- app/components/home/about.tsx
+++ app/components/home/about.tsx
@@ -3,7 +3,7 @@ import DOMPurify from "isomorphic-dompurify";
 
 export default function About() {
   return (
-    <section className="home-about container d-flex justify-content-center align-items-center" style={{minHeight: "40dvh"}}>
+    <section className="home-about container d-flex justify-content-center align-items-center py-5 my-5">
       <div className="col-10 col-md-10 col-lg-8">
         <h2 className="display-6 fw-bold text-primary">
           A Web <span className="text-accent-primary">Design</span> and <span className="text-accent-secondary">Development</span> Company
